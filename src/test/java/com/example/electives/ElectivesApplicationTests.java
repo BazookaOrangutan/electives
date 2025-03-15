c90e8e59c3a73507e452a3632ab5410ed153a672
Nikolay <kolyache280@gmail.com>
2025-03-15 09:02:30 +0300

    начальная структура

---
+++ src/test/java/com/example/electives/ElectivesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectivesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
